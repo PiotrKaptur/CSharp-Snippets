d25f04a8baa536e9b88a66c036e4cffb4ee5bdb5
Piotr Kaptur <piotr.kaptur.contact@gmail.com>
2025-07-07 07:24:53 +0200

    Add console menu for choosing factorial calculation method

--- Exercises/Task-Silnia/Program.cs
+++ Exercises/Task-Silnia/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Task_Silnia
+﻿using System.Numerics;
+
+namespace Task_Silnia
 {
     internal class Program
     {
@@ -28,17 +30,52 @@ Wyświetla wynik w konsoli.
                 return;
             }
 
-            if(number < 0)
+            if (number < 0)
             {
                 Console.WriteLine("Podana liczba jest ujemna!");
                 return;
             }
-            ulong silnia = 1;
-            for(ulong i = 1; i <= number; i++)
+
+            Console.WriteLine("Wybierz opcję obliczania silni:");
+            Console.WriteLine("1. Iteracyjnie");
+            Console.WriteLine("2. Rekurencyjnie");
+            Console.WriteLine("Podaj swój wybór: ");
+
+            var choice = Console.ReadLine();
+
+            switch (choice)
             {
-                silnia *= i;
+                case "1":
+                    {
+
+                        FunkcjaIteracyjna(number);
+                        Console.WriteLine($"Silnia z liczby {number} wynosi: {FunkcjaIteracyjna(number)}");
+                        
+
+                        break;
+                    }
+                case "2":
+                    {
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Nieznana opcja!");
+                        return;
+                    }
             }
-            Console.WriteLine($"Silnia dla {number} wynosi {silnia}!");
         }
+
+        static BigInteger FunkcjaIteracyjna(ulong number)
+        {
+            BigInteger result = 1;
+            for (ulong i = 1; i <= number; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+
     }
 }
