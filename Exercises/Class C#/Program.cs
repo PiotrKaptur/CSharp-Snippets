82bdebb1da37ca287598c2c37dcd23e4ea3308ab
Piotr Kaptur <piotr.kaptur.contact@gmail.com>
2025-07-08 23:06:39 +0200

    Add task (Class C#)

---
+++ Exercises/Class C#/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Class_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Exercises/Class C#/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Student
+{
+	public string FirstName { get; set; }
+	public string LastName { get; set; }
+	public List<int> Grades { get; set; }
+
+	//Konstruktor
+	public Student(string firstName, string lastName)
+	{
+		FirstName = firstName;
+		LastName = LastName;
+		Grades = new List<int>();
+	}
+
+	// Metody:
+
+	public void AssGrade(int grade)
+	{
+		if(grade < 1 || grade > 5)
+		{
+			throw new ArgumentOutOfRangeException(nameof(grade), "Ocena musi być w zakresie od 1 - 5.");
+		}
+        //W przeciwnym razie dodajemy ocenę
+        Grades.Add(grade);
+    }
+}
